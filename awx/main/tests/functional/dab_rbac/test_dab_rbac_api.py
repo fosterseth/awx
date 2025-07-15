8b5158acdb8aee2300f261570e66c1394b8072a5
jessicamack <jmack@redhat.com>
2025-07-15 21:50:01 +0200

    fix broken test

--- awx/main/tests/functional/dab_rbac/test_dab_rbac_api.py
+++ awx/main/tests/functional/dab_rbac/test_dab_rbac_api.py
@@ -172,9 +172,7 @@ def test_adding_actor_to_platform_roles(setup_managed_roles, role_name, actor, o
     - Team cannot be added to Organization Member or Admin role
     - Team cannot be added to Team Admin or Team Member role
     '''
-    if actor == 'team' and 'Organization' in role_name:
-        expect = 400
-    elif actor == 'team' and 'Team' in role_name:
+    if actor == 'team':
         expect = 400
     else:
         expect = 201
@@ -186,7 +184,8 @@ def test_adding_actor_to_platform_roles(setup_managed_roles, role_name, actor, o
     actor_id = bob.id if actor == 'user' else team.id
     data[actor] = actor_id
     r = post(url, data=data, user=admin, expect=expect)
-    if 'Organization' in role_name:
-        assert 'Assigning organization member permission to teams is not allowed' in str(r.data)
-    if 'Team' in role_name:
-        assert 'Assigning team permissions to other teams is not allowed' in str(r.data)
+    if expect == 400:
+        if 'Organization' in role_name:
+            assert 'Assigning organization member permission to teams is not allowed' in str(r.data)
+        if 'Team' in role_name:
+            assert 'Assigning team permissions to other teams is not allowed' in str(r.data)
