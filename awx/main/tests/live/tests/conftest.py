04d4ae28abf4f0d7ec2ba5da1dc414c057ec774b
Dirk Julich <djulich@redhat.com>
2026-02-18 16:11:25 +0100

    AAP-58470 integration tests (core) for external queries (#7278)
    * Add collection for testing external queries
    
    * Add query files for testing external query file runtime integration
    
    * Add live tests for external query file runtime integration
    
    * Remove redundant wait for events and refactor test data folders

--- awx/main/tests/live/tests/conftest.py
+++ awx/main/tests/live/tests/conftest.py
@@ -22,6 +22,7 @@ logger = logging.getLogger(__name__)
 
 
 PROJ_DATA = os.path.join(os.path.dirname(data.__file__), 'projects')
+COLL_DATA = os.path.join(os.path.dirname(data.__file__), 'collections')
 
 
 def _copy_folders(source_path, dest_path, clear=False):
@@ -53,6 +54,7 @@ def live_tmp_folder():
         shutil.rmtree(path)
     os.mkdir(path)
     _copy_folders(PROJ_DATA, path)
+    _copy_folders(COLL_DATA, path)
     for dirname in os.listdir(path):
         source_dir = os.path.join(path, dirname)
         subprocess.run(GIT_COMMANDS, cwd=source_dir, shell=True)
